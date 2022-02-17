9adbeba9cfcb1f57f6198af9dd9bc1ede6dbf3f9
Shynggys Shynbolatov (S.CECS) <21shynbolatov.s@vinuni.edu.vn>
2022-02-17 17:18:35 +0700

    everything ready except db insert fix

--- crawler.js
+++ crawler.js
@@ -1,6 +1,15 @@
 import { cheerio } from "https://deno.land/x/cheerio@1.0.4/mod.ts";
+import { FileDB } from "https://deno.land/x/filedb/mod.ts";
 
-const base_url = 'https://mitadmissions.org/blogs/';
+const db = new FileDB({ rootDir: "./data", isAutosave: true }); // create database with autosave
+
+const socialMediaLinksDB = await db.getCollection("socialMediaLinks");
+const emailsDB = await db.getCollection("emails");
+const telPhoneNumbersDB = await db.getCollection("telPhoneNumbers");
+
+// TODO: Edit here
+const initial_url = 'https://devdojo.com/';
+const base_url = 'devdojo.com';
 
 let all_links = new Set();
 
@@ -27,8 +36,12 @@ const socialMediaRegex = {
 
 let socialMediaLinks = new Set();
 let emails = new Set();
+let phoneNumbers = new Set();
 
 async function crawl(url) {
+  if (!url.includes(base_url)) {
+    return;
+  }
   if (all_links.has(url)) {
     return;
   }
@@ -42,22 +55,33 @@ async function crawl(url) {
     const links = $('a');
     const links_length = links.length;
     for (let i = 0; i < links_length; i++) {
+      let media = false;
       for (let regex in socialMediaRegex) {
         if (socialMediaRegex[regex].test(links[i].attribs.href)) {
           socialMediaLinks.add(links[i].attribs.href);
+          await socialMediaLinksDB.insertOne(
+            links[i].attribs.href
+          );
+          media = true;
+          break;
         }
       }
       
-      let urlRegEx = RegExp(/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm);
-      if (!urlRegEx.test(links[i].attribs.href)) {
-        if (links[i].attribs.href.includes('mailto:')) {
-          emails.add(links[i].attribs.href.replace('mailto:', ''));
-          continue;
-        } 
-        let full_link = base_url + links[i].attribs.href;
-        crawl(full_link);
-      } else {
-        crawl(links[i].attribs.href);
+      if (!media) {
+        let urlRegEx = RegExp(/^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm);
+        if (!urlRegEx.test(links[i].attribs.href)) {
+          if (links[i].attribs.href.includes('mailto:')) {
+            emails.add(links[i].attribs.href.replace('mailto:', ''));
+            continue;
+          } else if (links[i].attribs.href.includes('tel:')) {
+            phoneNumbers.add(links[i].attribs.href.replace('tel:', ''));
+            continue;
+          }
+          let full_link = initial_url + (links[i].attribs.href).replace(/^\/+/, '');
+          crawl(full_link);
+        } else {
+          crawl(links[i].attribs.href);
+        }
       }
     }
 
@@ -66,4 +90,4 @@ async function crawl(url) {
   }
 }
 
-await crawl(base_url);
+await crawl(initial_url);
