f20300270c6939155cdb52a8be0a4844f0f23754
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2022-03-23 19:39:27 +0700

    Update puppyteer_scraper.js

--- puppyteer_scraper.js
+++ puppyteer_scraper.js
@@ -1,23 +1,37 @@
 import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+import * as XLSX from 'https://deno.land/x/sheetjs/xlsx.mjs'
+import * as cptable from 'https://deno.land/x/sheetjs/dist/cpexcel.full.mjs';
+XLSX.set_cptable(cptable);
+let workbook = XLSX.utils.book_new();
+try { await Deno.remove('./projects/data/emails.xlsx'); } catch (error) {}
 
-let urls = {
-  initial_url: "https://www.irf.se/en/contact-kiruna/",
-  base_domain: "irf.se",
+const browser = await puppeteer.launch({
+  headless: false,
+});
+const page = await browser.newPage();
+await page.setDefaultNavigationTimeout(0); 
+
+let urls_initial = {
+  initial_url: "http://www.eie.gr/index-en.html",
+  base_domain: "eie.gr",
 }
+
+let visited_links = new Set();
+let sheet_id = 0;
+let emails_global = new Set();
 
 async function crawl(urls) {
 
   await page.goto(urls["initial_url"], {
-    waitUntil: "networkidle2",
+    // waitUntil: "networkidle2",
+    timeout: 0,
   });
   // await page.screenshot({ path: "screenshot.png" });
   console.log("Started");
   let emails_with_text_and_nav_links = await page.evaluate(() => {
     let links = document.getElementsByTagName("a");
-    let text_around = [];
+    let texts_around = [];
     let emails = [];
     let nav_links = [];
 
@@ -27,20 +41,57 @@ async function crawl(urls) {
       if (link.href.includes("mailto:")) {
         let email = link_href.split("mailto:")[1];
         emails.push(email);
-        text_around.push(link.parentNode.parentNode.parentNode.textContent);
+        let text_around = link.parentNode.parentNode.textContent;
+        text_around = text_around.replace(/[^a-zA-Z ]/g, ""); 
+        texts_around.push(text_around);
       } else {
         nav_links.push(link_href);
       }
     }
     return {
       emails,
-      text_around,
+      texts_around,
       nav_links
     };
   });
 
-  console.log(emails_with_text_and_nav_links);
+  // saving to excel
+  let emails_with_text = [];
+  let emails_to_check_for_emptiness = [];
+  for (let i = 0; i < emails_with_text_and_nav_links["emails"].length; i++) {
+    if (!emails_global.has(emails_with_text_and_nav_links["emails"][i])) {
+      emails_with_text.push({
+        email:       emails_with_text_and_nav_links["emails"][i],
+        text_around: emails_with_text_and_nav_links["texts_around"][i],
+      });
+      emails_to_check_for_emptiness.push(emails_with_text_and_nav_links["emails"][i]);
+      emails_global.add(emails_with_text_and_nav_links["emails"][i]);
+    }
+  }
+  if (emails_to_check_for_emptiness.length > 0) {
+    console.log(emails_to_check_for_emptiness);
+    let ws = XLSX.utils.json_to_sheet(emails_with_text);
+    let sheet_name = "Contacts_" + sheet_id;
+    XLSX.utils.book_append_sheet(workbook, ws, sheet_name);
+    
+    XLSX.writeFile(workbook, "./projects/data/emails.xlsx", {type: 'file'});
+  }
+  sheet_id += 1;
+  // recursive crawls
+  for (const link of emails_with_text_and_nav_links["nav_links"]) {
+    console.log(link);
+    if (!visited_links.has(link) && link.includes(urls["base_domain"])) {
+      visited_links.add(link);
+
+      await crawl({
+        initial_url: link,
+        base_domain: urls["base_domain"],
+      });
+    }
+  }
 }
 
-await crawl(urls);
+
+
+await crawl(urls_initial);
 await browser.close();
